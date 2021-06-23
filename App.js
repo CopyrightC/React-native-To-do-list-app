2aee65a1514ca406d7022795f2c7386fa5216a8f
Shourya <shouryasinha001@gmail.com>
2021-06-23 16:48:12 +0530

    initialised repo

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -15,7 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
