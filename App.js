5c0f713ab59d853d3546f2b595e62930197e6aed
Shourya <shouryasinha001@gmail.com>
2021-06-23 19:38:11 +0530

    UI ready

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Tasks from './components/tasks';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+
+        <View style = {styles.app}>
+        <Text style = {styles.header}> Pending tasks </Text>
+
+        <Tasks/>
+
+        </View>
+    
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#fff',    
   },
+  app : {
+    paddingTop : 60,
+    paddingHorizontal : 20,
+  },
+  header : {
+    fontSize : 20,
+    fontWeight : "bold",
+  },
+
+  lists : {
+
+  }
+
 });
---
+++ components/tasks.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {View , Text, StyleSheet, TouchableOpacity} from "react-native";
+
+
+
+export default function Tasks (props) {
+    return(
+        <View style = {styles.item}>        
+            <View style = {styles.itemMain}>
+                <TouchableOpacity style = {styles.square}>
+
+                </TouchableOpacity>
+                <Text>This is tasks.js {props.text}</Text>
+            </View>
+            <View style = {styles.select}></View>
+        </View>
+
+    )
+
+}
+
+const styles = StyleSheet.create({
+    item : {
+        backgroundColor : "#E3E9EE",
+        padding : 15,
+        borderRadius : 10,
+        flexDirection : "row",
+        alignItems : "center",
+        justifyContent : "space-between",
+        marginBottom : 20,
+        marginTop : 20,
+    },
+
+    itemMain : {
+        flexDirection : "row",
+        alignItems : "center",
+        flexWrap : "wrap",
+    },
+
+    square : {
+        width : 20,
+        height : 20,
+        backgroundColor : "#8BBBCB",
+        borderRadius : 5,
+        marginRight : 15,
+    },
+    select : {
+        width : 12,
+        height : 12,
+        borderColor : "#997980",
+        borderWidth : 2,
+        borderRadius : 4,
+        
+    },
+})
